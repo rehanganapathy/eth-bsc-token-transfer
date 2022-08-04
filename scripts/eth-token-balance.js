6b8654afc6bdde9646a159d66a2e5f4c62d4ac8a
Rehan Ganapathy <79349712+rehanganapathy@users.noreply.github.com>
2022-08-04 20:09:54 +0530

    Update eth-token-balance.js

--- scripts/eth-token-balance.js
+++ scripts/eth-token-balance.js
@@ -1,4 +1,4 @@
-const TokenEth = artifacts.require('/Users/rehanganapathy/Desktop/cross-chain/smart_contracts/TokenEth.sol');
+const TokenEth = artifacts.require('./TokenEth.sol');
 
 module.exports = async done => {
   const [sender, _] = await web3.eth.getAccounts();
